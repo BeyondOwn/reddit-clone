9bc3d6c56487f0083c963273837fa02fca020ab9
BeyondOwn <beyondown@gmail.com>
2024-07-19 15:37:22 +0300

    custom feed improvements

--- src/components/CustomFeed.tsx
+++ src/components/CustomFeed.tsx
@@ -5,6 +5,7 @@ import { getAuthSession } from "@/lib/auth"
 
 const CustomFeed = async () =>{
 
+    let combinedPosts = [];
     const session = await getAuthSession()
 
    const followedCommunities = await db.subscription.findMany({
@@ -35,9 +36,38 @@ const CustomFeed = async () =>{
     },
     take: INFINITE_SCROLLING_PAGINATION_RESULTS,
    })
+
+   if(posts.length < INFINITE_SCROLLING_PAGINATION_RESULTS){
+    const allOtherPosts = await db.post.findMany({
+        where:{
+            subreddit:{
+                name:{
+                    notIn: followedCommunities.map(({subreddit})=>subreddit.id),
+                },
+            },
+        },
+        orderBy:{
+            createdAt:'desc',
+        },
+        include:{
+            votes:true,
+            author:true,
+            comments:true,
+            subreddit:true,
+        },
+        take:INFINITE_SCROLLING_PAGINATION_RESULTS,
+    })
+
+    combinedPosts = allOtherPosts
+    }
+
+    else {
+        combinedPosts = posts;
+    }
+   
    
 
-    return <PostFeed initialPosts={posts} />
+    return <PostFeed initialPosts={combinedPosts} />
 }
 
 export default CustomFeed
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import { db } from "./db";
 import {PrismaAdapter} from '@next-auth/prisma-adapter'
 import GoogleProvider from 'next-auth/providers/google'
 import { nanoid } from 'nanoid'
-import { redirect } from "next/dist/server/api-utils";
+
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(db),
